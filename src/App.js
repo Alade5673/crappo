6c521a72cc10aedaf036481fc9b61d413a9a14fd
FALANA ELIJAH <falanaelijah@FALANAs-MacBook-Pro.local>
2021-10-29 13:35:58 +0100

    helmet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Helmet } from 'react-helmet';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Helmet>
+        <title>Crappo</title>
+        <meta
+          name="description"
+          content="Crypto Wallet"
+        />
+      </Helmet>
+
+        <div>
+          <Home />
+        </div>
+    </BrowserRouter>
+    
+    
   );
 }
 
